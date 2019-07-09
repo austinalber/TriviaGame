0a13ab84c71acab73913ea66cbe9b023b8eb8acb
Austin Alber <austinalber@gmail.com>
2019-07-09 13:45:06 -0400

    Changed questions to match Disney

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,31 +18,31 @@ var trivia = {
 
     // questions options and answers data
     questions: {
-        q1: 'Who is actually a chef?',
-        q2: 'What does Joey love to eat?',
-        q3: 'How many times has Ross been divorced?',
-        q4: 'How many types of towels does Monica have?',
-        q5: "Who stole Monica's thunder after she got engaged?",
-        q6: 'Who hates Thanksgiving?',
-        q7: "Who thinks they're always the last to find out everything?"
+        q1: "What does Melody, Ariel's daughter, (The Little Mermaid II: Return to the Sea) want to become?",
+        q2: "Where does Mr. Fredricksen's wife, (Up) want to live near?",
+        q3: "What's on the the pin bottle cap that Carl gives Russell, (Up)?",
+        q4: 'What kind of fish are Marlin and Nemo? (Finding Nemo)',
+        q5: "Who is Lilo's (Lilo & Stitch) favorite singer?",
+        q6: 'What is used to power the city at the end of Monsters, Inc?',
+        q7: "What is the first 'true' original Disney Cartoon?"
     },
     options: {
-        q1: ['Monica', 'Chandler', 'Rachel', 'Ross'],
-        q2: ['Fish', 'Apples', 'Oranges', 'Sandwhiches'],
-        q3: ['5', '2', '1', '3'],
-        q4: ['3', '8', '11', '6'],
-        q5: ['Rachel','Phoebe','Emily','Carol'],
-        q6: ['Joey','Chandler','Rachel','Ross'],
-        q7: ['Ross', 'Phoebe', 'Monica','Chandler']
+        q1: ['Sea Witch', 'Mermaid', 'Lobster', 'Flounder'],
+        q2: ['Paradise Falls', 'Victoria Falls', 'Machu Picchu', 'Seven Falls'],
+        q3: ['Grape Soda', 'Stawberry Soda', 'Lemon Soda', 'You Did It!'],
+        q4: ['Flounder', 'Oragne-Striped', 'Clownfish', 'Tangs'],
+        q5: ['The Beatles','Jerry Lee Lewis','Elvis Presley','Ritchie Valens'],
+        q6: ['Fear','Scream','Electricity','Laughter'],
+        q7: ['Dumbo', 'Bambi', 'Peter Pan','The Lion King']
     },
     answers: {
-        q1: 'Monica',
-        q2: 'Sandwhiches',
-        q3: '3',
-        q4: '11',
-        q5: 'Rachel',
-        q6: 'Chandler',
-        q7: 'Phoebe'
+        q1: 'Mermaid',
+        q2: 'Paradise Falls',
+        q3: 'Grape Soda',
+        q4: 'Clownfish',
+        q5: 'Elvis Presley',
+        q6: 'Laughter',
+        q7: 'The Lion King'
     },
 
     // trivia methods
@@ -60,7 +60,7 @@ var trivia = {
         $('#game').show();
         
         //  empty last results
-        $('#results').html('');
+        $('#results').empty();
         
         // show timer
         $('#timer').text(trivia.timer);
@@ -117,17 +117,17 @@ var trivia = {
             trivia.result = false;
             clearInterval(trivia.timerId);
             resultId = setTimeout(trivia.guessResult, 1000);
-            $('#results').html('<h3>Out of time! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+            $('#results').html('<h3>Out of time! The answer was ' + Object.values(trivia.answers)[trivia.currentSet] + '</h3>');
         }
         // if all the questions have been shown end the game, show results
         else if(trivia.currentSet === Object.keys(trivia.questions).length){
             
             // adds results of game (correct, incorrect, unanswered) to the page
             $('#results')
-            .html('<h3>Thank you for playing!</h3>'+
-            '<p>Correct: '+ trivia.correct +'</p>'+
-            '<p>Incorrect: '+ trivia.incorrect +'</p>'+
-            '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+            .html('<h3>Thank you for playing!</h3>' +
+            '<p>Correct: '+ trivia.correct + '</p>' +
+            '<p>Incorrect: '+ trivia.incorrect + '</p>' +
+            '<p>Unaswered: '+ trivia.unanswered + '</p>' +
             '<p>Please play again!</p>');
             
             // hide game sction
@@ -165,7 +165,7 @@ var trivia = {
             trivia.incorrect++;
             clearInterval(trivia.timerId);
             resultId = setTimeout(trivia.guessResult, 3000);
-            $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
+            $('#results').html('<h3>Better luck next time! The correct answer is: '+ currentAnswer +'</h3>');
         }
     },
 
